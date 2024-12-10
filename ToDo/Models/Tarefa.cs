fce3f3079b48a34676f6430c7bb87707d77c3f2f
CristianGrigorita230000287 <230000287@esg.ipsantarem.pt>
2024-12-10 20:16:19 +0000

    Tarefas - CRUD, tudo a funcionar. Configuração do ficheiro _Layout. Adaptação das Views das pastas Categorias e Tarefas ao Layout. Correção de um erro no models Tarefa

--- ToDo/Models/Tarefa.cs
+++ ToDo/Models/Tarefa.cs
@@ -3,12 +3,12 @@
     public class Tarefa
     {
         public int Id { get; set; }
-        public int UtilizadorId {  get; set; }
+        public int? UtilizadorId { get; set; }
         public string Nome { get; set; }
-        public string Prioridade { get; set; }
-        public string Estado { get; set; }
-        public int CategoriaId { get; set; }
-        public DateTime DataCriacao { get; set; }
-        public DateTime DataLimite { get; set; }
+        public string? Prioridade { get; set; }
+        public string? Estado { get; set; }
+        public int? CategoriaId { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public DateTime? DataLimite { get; set; }
     }
 }
